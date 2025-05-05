9f82a8ee9541a09583c25acfebb27deb781e0fc8
mtaoufik <mohamed.taoufik@cgi.com>
2025-05-05 15:26:03 +0300

    Update test_deepseek_agent.py

--- tests/test_deepseek_agent.py
+++ tests/test_deepseek_agent.py
@@ -5,12 +5,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../'
 from deepseek_agent import DeepSeekAgent
 class TestDeepSeekAgent(unittest.TestCase):
     def test_model_loading(self):
-        agent = DeepSeekAgent('../models/deepseek_model.h5')
+        agent = DeepSeekAgent('/models/deepseek_model.h5')
         self.assertIsNotNone(agent.model)
 
     def test_prediction(self):
-        agent = DeepSeekAgent('../models/deepseek_model.h5')
-        input_data = [0.5, 1.2, 3.4, 2.1, 0.8, 1.5, 2.3, 0.9]
+        agent = DeepSeekAgent('/models/deepseek_model.h5')
+        input_data = [1.5, 1.9, 5.4, 5.1, 5.8, 5.5, 5.3, 3.9]
         prediction = agent.predict(input_data)
         self.assertEqual(len(prediction), 1)
 
